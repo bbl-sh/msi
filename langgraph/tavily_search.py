581732fd7eb38c5683b04e87e64a2ec1c64c1da5
john221wick <bhushanbharat6958@gmail.com>
2024-08-11 22:38:09 +0530

    sunday

---
+++ langgraph/tavily_search.py
@@ -0,0 +1,62 @@
+from typing import Annotated
+from langchain_ollama.llms import OllamaLLM
+
+from typing_extensions import TypedDict
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from dotenv import load_dotenv
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langgraph.prebuilt import ToolNode, tools_condition
+load_dotenv()
+from langchain_experimental.llms.ollama_functions import OllamaFunctions
+from langchain_core.messages import BaseMessage
+
+llm = OllamaFunctions(model="llama3.1")
+
+# llm = OllamaLLM(
+#     model = "llama3.1"
+# )
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+
+graph_builder = StateGraph(State)
+
+tool = TavilySearchResults(max_results=1)
+tools = [tool]
+
+llm_with_tools = llm.bind_tools(tools)
+
+def chatbot(state: State):
+    messages = [message.content for message in state["messages"]]
+    return {"messages":[llm_with_tools.invoke(messages)]}
+
+graph_builder.add_node("chatbot", chatbot)
+
+tool_node = ToolNode(tools=[tool])
+graph_builder.add_node("tools", tool_node)
+
+graph_builder.add_conditional_edges(
+    "chatbot",
+    tools_condition,
+)
+# Any time a tool is called, we return to the chatbot to decide the next step
+graph_builder.add_edge("tools", "chatbot")
+graph_builder.set_entry_point("chatbot")
+graph_builder.add_edge("chatbot", END)
+graph_builder.set_finish_point("chatbot")
+
+graph = graph_builder.compile()
+
+
+
+while True:
+    user_input = input("User: ")
+    if user_input.lower() in ["quit", "exit", "q"]:
+        print("Goodbye!")
+        break
+    for event in graph.stream({"messages": [("user", user_input)]}):
+        for value in event.values():
+            if isinstance(value["messages"][-1], BaseMessage):
+                print("Assistant:", value["messages"][-1].content)
--- langgraph/test.py
+++ langgraph/test.py
@@ -1,19 +1,34 @@
-from langchain_core.prompts import ChatPromptTemplate
+from typing import Annotated
 from langchain_ollama.llms import OllamaLLM
-from langchain import hub
-from langchain.agents import AgentExecutor, create_react_agent
+
+from typing_extensions import TypedDict
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
 
 llm = OllamaLLM(
-    model = "qwen2:0.5b"
+    model = "llama3.1"
 )
 
-messages = [
-    ("system", "you are a good ai"),
-    ("human", "tell me about {question}")
-]
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
 
-prompts = ChatPromptTemplate.from_messages(messages)
 
-chain = prompts | llm
+graph_builder = StateGraph(State)
 
-print(chain.invoke({"question": "What is LangChain?"}))
+def chatbot(state: State):
+    return {"messages": [llm.invoke(state["messages"])]}
+
+graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_edge("chatbot", END)
+
+graph = graph_builder.compile()
+
+while True:
+    user_input = input("User: ")
+    if user_input.lower() in ["quit", "exit", "q"]:
+        print("Goodbye!")
+        break
+    for event in graph.stream({"messages": ("user", user_input)}):
+        for value in event.values():
+            print("Assistant:", value["messages"][-1])
